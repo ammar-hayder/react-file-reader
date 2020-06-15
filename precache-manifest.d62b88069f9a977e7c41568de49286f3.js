b2e71bcf397eaa9fd7622e3396187899c825d19d
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-06-15 06:07:38 +0000

    Deploy ammar-hayder/react-file-reader to github.com/ammar-hayder/react-file-reader.git:gh-pages

---
+++ precache-manifest.d62b88069f9a977e7c41568de49286f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a75bd067986f646966b9d18348bbf623",
+    "url": "/react-file-reader/index.html"
+  },
+  {
+    "revision": "ddb5da05647db8387091",
+    "url": "/react-file-reader/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f826c1eb89429181f5e3",
+    "url": "/react-file-reader/static/js/2.9cb379bd.chunk.js"
+  },
+  {
+    "revision": "dde2f98dd0ed8bfad18d88defdcf5f43",
+    "url": "/react-file-reader/static/js/2.9cb379bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ddb5da05647db8387091",
+    "url": "/react-file-reader/static/js/main.a9fe4974.chunk.js"
+  },
+  {
+    "revision": "18bdb1a7aeb06a4c9fc2",
+    "url": "/react-file-reader/static/js/runtime-main.20dbd041.js"
+  }
+]);
